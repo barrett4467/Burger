45c0b051cf6e34899b6743ee56fcc207ec979685
Rebecca Barrett <barrett4467@gmail.com>
2019-10-17 21:07:42 -0600

    added jaws

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 //connection to database
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "becca",
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "becca",
+        database: "burgers_db"
+    });
+}
 //sets up the actual connection
 connection.connect(function(err){
     if (err) throw err;
